57d1b60b77ad17282cd84a71063f435fa92b44a8
vsoch <vsochat@stanford.edu>
2015-10-30 13:34:07 -0700

    modified:   doc/source/conf.py 	deleted:    index.html

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -12,9 +12,19 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
+from unittest.mock import MagicMock
 import sys
 import os
 import shlex
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return Mock()
+
+MOCK_MODULES = ['pycurl']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
