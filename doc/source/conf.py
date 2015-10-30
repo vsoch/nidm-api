d8f1b7d20a00f8a9b28fcd9ad93c5fcd271344d2
vsoch <vsochat@stanford.edu>
2015-10-30 14:03:32 -0700

    testing fixing docs

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -22,9 +22,8 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['pycurl']
+MOCK_MODULES = ['pycurl','rdflib']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
-
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
