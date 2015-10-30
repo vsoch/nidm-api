8d906dda50bcd2b2b7245278aeb1169f83875503
vsoch <vsochat@stanford.edu>
2015-10-30 13:22:59 -0700

    removing added dependencies that were from nidmresults

--- nidm/results.py
+++ nidm/results.py
@@ -1,8 +1,6 @@
 '''
 results: part of the nidm-api
-functions for working with nidm results:
-export of neuroimaging statistical results using NIDM as specified at 
-http://nidm.nidash.org/specs/nidm-results.html.
+functions for working with nidm results
 
 '''
 
